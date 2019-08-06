0966e93191b9bb728f149f4398cc86a95ccc3ac6
javaDart <jkomenan@julaya.co>
2019-08-06 17:07:27 +0000

    Morpion Game

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# MorpionMJ_Tic-Tac-Toe
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+
+## App : Jeu de morpion
+creation d'un jeu de morpion interactif avec ReactJS.
+
+##Indications
+Ce jeu
+ .vous permet de jouer au  (Tic-Tac-Toe),
+ .indique quel joueur a remporté la partie,
+ .stocke un historique de jeu au fil des tours,
+ .permet aux joueurs de revenir à un point quelconque de l’historique.
+
+##Visitez moi ici [https://javadart.github.io]
---
+++ src/index.js
@@ -0,0 +1,165 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import swal from '@sweetalert/with-react';
+import "./index.css";
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div className="board">
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+        {/* <ol>{movess}</ol> */}
+      </div>
+    );
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>
+          Powered by <a href="https://javadart.github.io">Juslin</a>
+        </p>
+      </footer>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "M" : "J";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
+  }
+
+  render() {
+    const ticTacToe = "MORPION GAME with ReactJS";
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move
+        ? "Revenir au tour N° " + move
+        : "Revenir au debut de la partie";
+      return (
+        <li className="part-list" key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+    let status;
+    if (winner) {
+      status = winner + " a gagné !";
+      swal(status + " a gagné !");
+    } else {
+      status = "Prochain joueur : " + (this.state.xIsNext ? "M" : "J");
+    }
+    return (
+      <div className="game-wrapper">
+        <h1>{ticTacToe}</h1>
+        <div className="game-board">
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
+        </div>
+
+        <div className="footer">
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+}
+
+// ======================================== //
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+// declarer un vainqueur
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
